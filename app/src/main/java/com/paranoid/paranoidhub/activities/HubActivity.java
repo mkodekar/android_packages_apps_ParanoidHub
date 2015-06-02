85019f07326287ed75a149c448766a94a7dede61
ayysir <ayysir@aospal.com>
2015-06-02 01:47:31 -0400

    Hub: drawer header animation to shake

--- app/src/main/java/com/paranoid/paranoidhub/activities/HubActivity.java
+++ app/src/main/java/com/paranoid/paranoidhub/activities/HubActivity.java
@@ -501,7 +501,7 @@ public class HubActivity extends AppCompatActivity
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType("image/*");
         startActivityForResult(intent, select_photo);
-        mDrawerImage.setAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in));
+        mDrawerImage.setAnimation(AnimationUtils.loadAnimation(this, R.anim.selector_shake));
     }
 
     @Override
@@ -538,7 +538,7 @@ public class HubActivity extends AppCompatActivity
                     case 1:
                         PreferenceHelper.removePreference(PreferenceHelper.PROPERTY_DRAWER_IMAGE);
                         mDrawerImage.setImageResource(R.drawable.drawer_bg);
-                        mDrawerImage.startAnimation(AnimationUtils.loadAnimation(mContext, R.anim.fade_in));
+                        mDrawerImage.startAnimation(AnimationUtils.loadAnimation(mContext, R.anim.default_shake));
                         break;
                 }
 
